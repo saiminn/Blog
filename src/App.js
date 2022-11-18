747ee03f25d9925f6f596ef8a18620d318334d69
saiminn <saiminnkhantkyaw.scholor@gmail.com>
2022-11-18 18:47:27 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Helmet>
+                <meta charSet="utf-8" />
+                <title>SM's Blog</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path='/'>
+              <Home/>
+          </Route>
+          <Route path='/create'>
+              <Create/>
+          </Route>
+          <Route path='/blogs/:id'>
+              <BlogDetails/> 
+          </Route>
+          <Route path='*'>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
