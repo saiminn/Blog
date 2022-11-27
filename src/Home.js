af656df054dd713391b7815a586e9001d28a2cef
saiminn <saiminnkhantkyaw.scholor@gmail.com>
2022-11-27 09:19:01 +0630

    commit

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import useFetch from "./useFetch";
 const Home = () => {
 
 
-  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+  const {data: blogs, isPending, error} = useFetch('https://my-json-server.typicode.com/saiminn/Blog/blogs')
   
 
 
